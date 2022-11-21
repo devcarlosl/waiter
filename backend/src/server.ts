dbe38e792d38ccf5aa8d69b80877169bfee56e68
Carlos Lopes <dev.carloslopes@gmail.com>
2022-11-21 01:17:28 -0300

    Adiciona as entidades Product, Category, Order, separa o express do server e separa o backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,15 @@
-import express from 'express';
 import mongoose from 'mongoose';
 
-const app = express();
+import { app } from './app';
+
 const PORT = 3001;
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is runing on http://localhost:${PORT}`);
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
   })
   .catch(() => {
     console.log('Error connecting to mongodb');
   });
-
-
