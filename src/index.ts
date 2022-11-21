8569a7f5d2a7e8434f4da6879f78c69d42d77a2a
Carlos Lopes <dev.carloslopes@gmail.com>
2022-11-20 23:45:51 -0300

    Adiciona o mongoose e conecta com o mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server is runing on http://localhost:${PORT}`);
-});
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is runing on http://localhost:${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log('Error connecting to mongodb');
+  });
+
+
